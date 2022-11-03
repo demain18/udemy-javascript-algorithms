6ee92e60d8d7d85efe81c1118fa6c7d892fb9fc2
dylanwaynehills <dylan@waynehills.co>
2022-11-03 22:55:18 +0900

    수정

--- 검색_알고리즘/선형_검색_소개.js
+++ 검색_알고리즘/선형_검색_소개.js
@@ -1,12 +1,13 @@
 function linearSearch(list, target) {
-  return (
-    list
-      .map((i, x) => {
-        if (i === target) return x;
-      })
-      .filter((i) => i !== undefined)[0] || -1
-  );
+  const result = list
+    .map((i, x) => {
+      if (i === target) return x;
+    })
+    .filter((i) => i !== undefined)[0];
+
+  return result !== undefined ? result : -1;
 }
 
 console.log(linearSearch([1, 3, 5], 3));
 console.log(linearSearch([1, 3, 5], 7));
+console.log(linearSearch([100], 100));
