25cc8402e380e2d6a328c1ed86b7090e17df55ec
백산 <hbfs0825@gmail.com>
2022-11-12 21:19:47 +0900

    선택 정렬 #1

--- 버블정렬/bubble.js
+++ 버블정렬/bubble.js
@@ -1,13 +1,13 @@
 function bubble(arr) {
   // result 변수를 선언한다.
-  let result = [];
+  const result = [];
   // 배열의 길이가 0보다 크다면 while문을 실행한다.
   while (arr.length > 0) {
     // arr에서 반복문을 돌린다.
     arr.map((i, x) => {
       // 1번과 2번 항목을 비교하고, 1번이 2번보다 더 크거나, 2번의 값이 undefined가 아니라면 값을 교환한다.
       if (arr[x] > arr[x + 1] && arr[x + 1] !== undefined) {
-        let tmp = arr[x + 1];
+        const tmp = arr[x + 1];
         arr[x + 1] = arr[x];
         arr[x] = tmp;
       }
---
+++ 선택정렬/selection.js
@@ -0,0 +1,3 @@
+function selection(arr) {}
+
+console.log(selection([23, 5, 15, 29, 38, 1, 2, 11, 30]));
